97fb2db68df8479cc5353a8ceb25e3a405def1fa
pudding <bdingtech@gmail.com>
2020-09-01 14:26:44 +0800

    init

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Logo from '../../assets/logo.png'
+
+class Header extends Component {
+    render(){
+        return (
+            <div id="header">
+                <div className="header-container">
+                    <div className="head-title">
+                        <img src={Logo} className="logo" alt="" ></img>
+                        <span>一键洗衣</span>
+                    </div>
+                    <div className="head-subtitle">
+                        <span>邱老板牛逼</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header;
